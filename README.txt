e99acb54dcd72110f29a48ede1e74f6fc63a539e
Ivan Schurawel <anz17n@gmail.com>
2018-03-27 00:09:27 +0900

    initial commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+This is a small python app to delay all the cards in a specific anki deck by 1 day. 
+You have to find the location of your library and use open the SQL database to 
+find the specific decks that you want to alter. I used SQLite.
+A lot of info about anki databases can be found: https://www.juliensobczak.com/tell/2016/12/26/anki-scripting.html
+
+This is extremely specific to the location of my anki database and decks.
+You will have to change them if you want to use this on your own.
+
+This is rough, but it's holding me over until I can find the time to turn this into
+an actual anki plugin.
---
+++ delay.py
@@ -0,0 +1,160 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  5 22:24:56 2017
+
+@author: Ivan
+
+
+info on the anki directory: https://www.juliensobczak.com/tell/2016/12/26/anki-scripting.html
+"""
+
+import sqlite3
+import shutil
+import datetime
+import time
+
+
+
+# deck 1450345949539 == Korean; 1461588996278 == countries; 1462459650256 == states
+
+
+def open_deck(deck='1450345949539'):
+    '''
+    str -> list (of tuples)
+    searches for all the cards from a given deck and returns it
+    '''
+    c.execute("SELECT * FROM cards WHERE did= ?", (deck,))
+    cardsFromDeck = c.fetchall()
+    return cardsFromDeck
+
+
+# for checking on if cards are due today
+todayDate = datetime.date.today()
+ankiCreateDate = datetime.date(2015, 4, 13)
+difference = (todayDate - ankiCreateDate).days #stripping time from the datetime
+
+def check_for_due(cards):
+    '''
+    list (of tuples) -> bool
+    goes through a list of cards from a deck and
+    varifies that something is due today
+    '''
+    # cards[[][8]] == due
+    for card in cards:
+        # if something's due today
+        if card[8] <= difference - 1: # -1 to prevent delaying down to 0 cards
+            return True
+    return False
+
+
+
+def not_new_cards(cards):
+    '''
+    list (of tuples) -> list (of tuples)
+    goes through a list of cards from a deck and
+    removes any cards that have not been studied yet
+    '''
+    notNewCards = []
+    # cards[[][6]] == type: 0=new, 1=learning, 2=due
+    for card in cards:
+        if card[6] == 0:
+            continue
+        notNewCards.append(card)
+    return notNewCards
+
+
+def delay(cards, days=1):
+    '''
+    list (of tuples), int -> list (of tuples)
+    goes through the list of cards and delays them
+    all by the given number of days
+    '''
+    # cards[[][0]] == (card) id
+    # cards[[][8]] == due
+    newList = []
+    for card in cards:
+        newList.append([card[0], (card[8] + days)])
+    return newList
+
+
+def update_database(cards):
+    '''
+    list -> ---
+    goes through each card id and updates the
+    database with the new due date
+    '''
+    # new indexes are [0] for id, and [1] for due date
+    with conn:  # replaces the need for "conn.commit()" at the end of the program
+        for card in cards:
+            c.execute("""UPDATE cards SET due= ?
+                      WHERE id= ?""", (card[1], card[0]))
+
+
+def run_program(decks):
+    '''
+    list -> ---
+    goes through each deck from the chosen
+    decks to delay and runs the steps for each deck
+    '''
+    for deckToRun in decks:
+        deck = open_deck(deckToRun)
+        cards = not_new_cards(deck)
+        if check_for_due(cards):
+            cards = delay(cards)
+            update_database(cards)
+            print('Cards in deck ' + str(deckToRun) + ' have been delayed.')
+        else:
+            print('No due cards in deck ' + str(deckToRun) + ' to delay')
+        
+
+def run():
+    '''
+    main execution of the program to delay cards
+    opens and closes the connection to the database
+    '''
+    # create backup of the collection before we edit anything
+    shutil.copy(r'/home/pi/Documents/Anki/User 1/collection.anki2', r'/home/pi/Documents/Anki/User 1/collection_backup.anki2')
+
+    # making these global so they can be within other function scopes
+    global conn
+    conn = sqlite3.connect(r'/home/pi/Documents/Anki/User 1/collection.anki2')
+    global c
+    c = conn.cursor()
+
+
+    # deck 1450345949539 == Korean; 1461588996278 == countries; 1462459650256 == states
+    run_program([1450345949539, 1461588996278, 1462459650256])
+    conn.close()
+    print('Process completed.')
+
+
+
+
+#get reset time to compare against
+resetTime = datetime.datetime(2015, 5, 8, 5, 0, 0)
+
+
+def getting_time():
+    while datetime.datetime.now().hour != resetTime.hour:
+        print("current hour is:", datetime.datetime.now().hour, ".. waiting 30 minutes...")
+        time.sleep(1800) # pausing for 30 mins to check again 
+    run()
+    print("Sleeping for 23 hours and 20 minutes..")
+    time.sleep(84000)
+
+
+print("Please close Anki before choosing an option.")
+option = input('Press "n" to dalay cards now, or "w" to wait. ').lower()
+
+while True:
+    if option == 'w':
+        getting_time()
+    elif option == 'n':
+        run()
+        break
+    else:
+        option = input('Press "n" to dalay cards now, or "w" to wait. ').lower()
+
+    
+
